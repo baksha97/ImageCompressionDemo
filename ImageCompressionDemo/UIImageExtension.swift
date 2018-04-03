7e07d2522decea71283f6356bd1aa0479bafcedb
baksha97 <baksha97@gmail.com>
2018-04-03 13:15:01 -0400

    initial commit

---
+++ ImageCompressionDemo/UIImageExtension.swift
@@ -0,0 +1,30 @@
+//
+//  UIImageExtension.swift
+//  ImageCompressionDemo
+//
+//  Created by Loaner on 4/3/18.
+//  Copyright © 2018 baksha97. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    //  The compression quality Enum allows us to keep our code clean rather than hard coding specific Float.
+    //  Layering our code in such a way allows us to spot errors early on when changes are made that could lead to inconsistencies.
+    enum JPEGCompressionQualityRate: CGFloat {
+        case lowest  = 0
+        case low     = 0.25
+        case medium  = 0.5
+        case high    = 0.75
+        case highest = 1
+    }
+    
+    /// Returns the data for the specified image in JPEG format.
+    //  We will return an image data object in JPEG format because UIImageJPEGRepresentation allows us to conviently compress our objects with a Float that is less than 1, but more than 0.
+    //  If a specific compression rate is consistently rathered, we can remove the enum and replace the parameter.
+    
+    func jpegData(compressed rate: JPEGCompressionQualityRate) -> Data? {
+        return UIImageJPEGRepresentation(self, rate.rawValue)
+    }
+}
--- ImageCompressionDemo/ViewController.swift
+++ ImageCompressionDemo/ViewController.swift
@@ -10,16 +10,129 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Image Picker Controller
+    // Image picker will be used to visualize choosing image.
+    let imagePicker = UIImagePickerController()
+    /*
+     If you are having an error output of:
+     Please ensure that you have included the proper properties in the Info.plist to access your device's components. 
+     {Error Domain=PlugInKit Code=13 "query cancelled" UserInfo={NSLocalizedDescription=query cancelled}"}
+     
+     Go to:
+     Product > Scheme > Edit Scheme > Environment Variables ---SET---> OS_ACTIVITY_MODE: disable
+     --- Seems to be due to the update to Swift 4 from Swift 3.
+     */
+    
+    //MARK: UI Components
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var menuButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //initalize delegation for the image picker
+        imagePicker.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    @IBAction func menuDidTap(_ sender: Any) {
+        openMenu()
+    }
+    
+    
+
+    func openMenu(){
+        // Create alert of type .alert
+        // If preffered, we may change to action sheet for a different type of interface view.
+        let alertController = UIAlertController(title: "Select Image",
+                                                message: "Choose Location of Image",
+                                                preferredStyle: .alert)
+        
+        
+        // Open Camera
+        let cameraAction = UIAlertAction(title: "Open Camera",
+                                         style: .default,
+                                         handler: { (action:UIAlertAction!) in
+                                            print ("Selecting photo from camera")
+                                            self.selectPhoto(from: .camera)
+        })
+        //Open Gallery
+        let galleryAction = UIAlertAction(title: "Choose Image From Gallery",
+                                          style: .default,
+                                          handler: { (action:UIAlertAction!) in
+                                            print ("Selecting photo from library")
+                                            self.selectPhoto(from: .photoLibrary)
+        })
+        
+        //Add actions to menu
+        alertController.addAction(cameraAction)
+        alertController.addAction(galleryAction)
+        
+        // Cancel action
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel,
+                                         handler: { (action:UIAlertAction!) in
+                                            print ("Cancel")
+        })
+        alertController.addAction(cancelAction)
+        
+        // Present Alert
+        self.present(alertController, animated: true, completion:nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+//Seperation of methods for this ViewController's protocols of UIImagePickerControllerDelegate & UINavigationControllerDelegate.
+//Allows use to increase readability of our view and allows reuse by simply refactoring the extension name to the specified view.
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    //parameter allows method to be reused with multiple sources.
+    func selectPhoto(from type: UIImagePickerControllerSourceType){
+        //crops photo to square to fit certain constraints; set to false if not prefered.
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = type
+        present(imagePicker, animated: true, completion: nil)
     }
+    
+    //Swift 3 -> 4 changes require @objc in function header for this selector.
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        //We will be running a debug test with our initial image to check compression & size reduction...
+        if let chosenImageData = chosenImage.jpegData(compressed: .highest){
+            print("initial image debug:")
+            print(chosenImageData.debugDescription)
+        }
+        ///^This test can be removed during implementation.
+        
+        
+        //Check/test to see if compressed image has been correctly compressed and does not equal nil (null)
+        if let compressedImageData = chosenImage.jpegData(compressed: .lowest) {
+            let compressedImage = UIImage(data: compressedImageData)!
+            print("compressed image debug:")
+            print(compressedImageData.debugDescription)
+            imageView.image = compressedImage
+            /*
+             IMPORTANT: In here, we would begin to perform any type of transaction for the image.
+             Example: Uploading it to a server or storing on a device.
+                When we store these types of data, we have the option of uploading the ImageData object itself, or we may convert it to a UIImage and then proceed.
+                */
+        }else{
+            //Possible because: The image has no data or if the CGImageRef bitmap format isn't supported.
+            print("The image has no data or if the CGImageRef bitmap format isn't supported.")
+        }
 
-
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //Optional but expected through Apple design guidelines
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion:nil)
+    }
 }
 
